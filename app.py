2d93a3228697e3a2ecd0d653a69c20d8db908ffd
Azeru <Azeru@users.noreply.github.com>
2020-02-27 20:26:08 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template
+import tensorflow as tf
+import numpy as np
+
+import time
+import PIL.Image
+from stylegan_two import StyleGAN, nImage
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+latent_size = 16
+
+@app.before_first_request
+def load_model_to_app():
+    app.predictor = StyleGAN()
+    app.predictor.load()
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    img_size =32
+    nSliders = latent_size
+    values = {}
+
+    for i in range(nSliders):
+        values["slider"+str(i)] = 50
+
+    if request.method == 'POST':
+        values = request.form
+        x = (np.array(list(values.values()), dtype="float32")/100).reshape((1, latent_size))
+    else:
+        x = np.ones((1, latent_size))*0.5
+    #imNoise stable np.ones((1, 32, 32, 1)) * 0.5
+    app.predictor.generateImage(x,nImage(1),save="static/image.jpg")
+    return render_template('index.html', nSliders=nSliders, val=values, size = img_size)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
